244c5705da4ca5162c992d261574060c2a0bf5c6
takehiro <takehiro19990420@icloud.com>
2019-12-18 10:52:41 +0000

    hoge

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -21,11 +21,12 @@ class ImageUploader < CarrierWave::Uploader::Base
     1..10.megabytes
   end
   
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+  # storage :fog
+  #else
+  # storage :fog
+  #end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
